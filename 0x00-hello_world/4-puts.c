e712995eea01e67197c533ff64334e66bd6d0eb5
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-03-17 00:00:44 +0100

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include<stdio.h>
 /**
   *main - Entry point
   *
@@ -6,7 +6,7 @@
   */
 int main(void)
 {
-	puts "Programming is like building a multilingual puzzle\n";
+	puts("\Programming is like building a multilingual puzzle");
 	return (0);
 }
 
