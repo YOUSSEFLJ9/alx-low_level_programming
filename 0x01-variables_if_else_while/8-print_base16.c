bc6f0cdf04223cf09362836d706dd96c7df06a0d
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-03-18 03:12:15 +0100

    done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - program that prints all single digit numbers of base 10
+ *
+ * Return: 0
+*/
+
+	int main(void)
+	{
+		char a = '0';
+		char b = 'a';
+
+		while (a <= '9')
+		{
+			putchar(i);
+			a++;
+		}
+		while (b < 'g')
+		{
+			putchar(k);
+			b++;
+		}
+			putchar('\n');
+
+		return (0);
+	}
