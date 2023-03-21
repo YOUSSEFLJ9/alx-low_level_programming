620dc20c202b47110c666234c979aafbe45a891b
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-03-21 18:28:05 +0000

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,10 @@ void print_alphabet_x10(void)
 	int times = 0;
 	char alph;
 
-	while(times<10 )
+	while (times < 10)
 	{
-		alph ='a';
-		while(alph <= 'z')
+		alph = 'a';
+		while (alph <= 'z')
 		{
 			_putchar(alph);
 			alph++;
