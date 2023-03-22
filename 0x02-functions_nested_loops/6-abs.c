88d30196ff63b33f529e2e2fdcc9ef78b28fcbb6
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-03-22 00:01:30 +0000

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *_abs - this function return the abslut Number of input
+  *
+  *Return: the abslut number
+  */
+int _abs(int n)
+{
+	if (n >0)
+		return (n);
+	else if (n < 0)
+		return (-n);
+
+	return (0);
+}
+
