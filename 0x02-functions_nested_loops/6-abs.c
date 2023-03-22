4b775d1aa0c86bf07702128c7e7b39557fb621e0
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-03-22 00:21:32 +0000

    done

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,10 +3,11 @@
   *_abs - this function return the abslut Number of input
   *
   *Return: the abslut number
+  *@n this number input
   */
 int _abs(int n)
 {
-	if (n >0)
+	if (n > 0)
 		return (n);
 	else if (n < 0)
 		return (-n);
