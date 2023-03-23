8ce03a7b905a5376410dddfe35c3b27c8c902086
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-03-23 13:55:06 +0000

    all is goood

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *print_numbers - this function print numbers 0 to 9
+  *
+  *Return: there is no return its void :)
+  *
+  */
+void print_numbers(void)
+{
+	int a = 48; 
+	while (a < 58)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+
+}
