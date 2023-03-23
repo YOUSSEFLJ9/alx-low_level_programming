357c4cf66f325b829ab766389335459b37897ecb
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-03-23 23:44:36 +0000

    i am so much sunshine in every square inch  lol

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  *print_square - this function print swuare 2d of #
+  *
+  *@size : represent the sizw of the square by colnes and lines
+  *
+  */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else if (size > 0)
+	{
+		int counter;
+
+		for (counter = 1; counter <= size; counter++)
+		{
+			int counterln;
+
+			for (counterln = 0; counterln <= size; counterln++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
