71a85aa13141c54fb0c8b8aaf1e72278d2ca3b7d
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-03-24 00:35:05 +0000

    FIZZBUZZ

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ *main - going to print from the fuction inside it
+  *
+  *Return: always 0
+  *
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+		printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
