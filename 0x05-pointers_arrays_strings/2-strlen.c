9b9dbddce49fa7a11f05c3d0fd0ea10abeb1ebad
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-03-27 22:41:38 +0000

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *_strlen - this fuction count the number of charachters
+  *
+  *@s: this is char counter
+  *
+  *Return: the lenght of the char (string)
+  */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s = '\0')
+	{
+		len++;
+	}
+	return (len);
+}
