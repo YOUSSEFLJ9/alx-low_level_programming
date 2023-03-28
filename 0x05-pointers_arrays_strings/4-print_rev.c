105aa3e4f9d2d7d28294653952f7be40cb806851
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-03-28 00:55:07 +0000

    can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,24 +1,23 @@
 #include "main.h"
+
 /**
- *print_rev - this function return reverse i string
- *
- *@s: this it the string
- *
- */
+  * print_rev - prints an str in reverse
+  * @s: the string
+  *
+  * Return: void
+  */
 void print_rev(char *s)
 {
+	int length = 0;
 
-	int lenght = 0;
-
-	while (s[lenght] != '\0')
+	while (*(s + length) != 0)
+		length++;
+	length--;
+	while (length >= 0)
 	{
-		lenght++;
-	}
-
-	while (lenght >= 0)
-	{
-		_putchar(s[lenght]);
-		lenght--;
+		_putchar(*(s + length));
+		length--;
 	}
 	_putchar('\n');
 }
+
