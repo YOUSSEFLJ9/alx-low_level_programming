37d9c57b6302ae464e3dd44b18da23c7fbff8a77
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-04 12:52:39 +0000

    0. memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_memset - this fuction full n byte of pointer by s
+ *@n: the Number of byte
+ *@s: the pointer to be return the changes
+ *@b: the string to be print
+ *
+ *Return: pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	s[n + 1] = '\0';
+	return (s);
+}
