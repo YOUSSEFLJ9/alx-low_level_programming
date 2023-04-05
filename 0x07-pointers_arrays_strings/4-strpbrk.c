7d942a438eba40b03d47ee1279912ad6f0d021bb
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-05 01:11:17 +0000

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,15 +10,14 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i;
 
-	while(*s)
+	while (*s)
 	{
 		for (i = 0; accept[i]; i++)
 		{
 			if (*s == accept[i])
 				return (s);
 		}
-		s++;
-		
+		s++;	
 	}
 	return ('\0');
 }
