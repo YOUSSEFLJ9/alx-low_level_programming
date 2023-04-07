f9e0e99cdde0d154f05ae221ce4a10707bdc6033
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-07 16:40:29 +0000

    uodate

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,12 +4,12 @@ int reversestring(char *s, int bg, int end);
 int _lenght(char *s, int ln);
 
 /**
-  * reversestring - this function reverses the string given
+  *reversestring - this function reverses the string given
   *
-  * @s: string variable
-  * @bg: int with the number of the bg of string
-  * @end: int hold the length of string
-  * Return: reverse of string
+  *@s: string variable
+  *@bg: int with the number of the bg of string
+  *@end: int hold the length of string
+  *Return: reverse of string
   */
 int reversestring(char *s, int bg, int end)
 {
@@ -24,9 +24,10 @@ int reversestring(char *s, int bg, int end)
 }
 
 /**
- * _lenght - return length of *s
- * @s: string
- * Return: length int
+ *_lenght - return length of *s
+ *@s: stringi
+ *@ln: lenght of s
+ *Return: length int
  */
 int _lenght(char *s, int ln)
 {
@@ -34,15 +35,15 @@ int _lenght(char *s, int ln)
 	if (s[ln] == '\0')
 		return (ln);
 	else
-		return ( _lenght(s , ln + 1));
+		return (_lenght(s, ln + 1));
 }
 
 /**
-  * is_palindrome - this function checks if the string can be read in two sides
+  *is_palindrome - this function checks if the string can be read in two sides
   *
-  * @s: this the string
+  *@s: this the string
   *
-  * Return: 1 if palindrome, 0 if not
+  *Return: 1 if palindrome, 0 if not
   */
 int is_palindrome(char *s)
 {
