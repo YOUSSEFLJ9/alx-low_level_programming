cae7279d9846e84b81f01fb5d00e3a4870e9550a
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-09 17:02:11 +0000

    first

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - this the main Entery
+  *
+  *@argc: int counter to count argument
+  *@argv: poiter of string (point to pointer char)
+  *
+  *Return: lets try 0
+  */
+
+int main(int argc, char **argv)
+{
+        while(argc--)
+                printf("%s\n", *argv++);
+        exit(EXIT_SUCCESS);
+}
