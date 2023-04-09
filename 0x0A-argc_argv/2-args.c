256e1109a3dbf0fc8b59f00881956c03b9902105
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-09 21:36:26 +0000

    task done

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,16 +3,18 @@
 #include <stdlib.h>
 /**
   *main - this the main Entery
-  *
   *@argc: int counter to count argument
   *@argv: poiter of string (point to pointer char)
+  *Return: lets try 0, in fact its print all the arg include the first one
   *
-  *Return: lets try 0
   */
-
 int main(int argc, char **argv)
 {
-	while (argc--)
-		printf("%s\n", *argv++);
+	int counter;
+
+	for (counter = 0; counter < argc ; counter++)
+	{
+		printf("%s\n", argv[counter]);
+	}
 	return (0);
 }
