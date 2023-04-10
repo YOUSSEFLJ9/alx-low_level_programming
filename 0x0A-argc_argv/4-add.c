c2f62cbf74b5d0e0c2e7b35730b316c3dc31f84d
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-10 17:43:44 +0000

    im doing good

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - this function sum positive numbers
+  *@argv: arg vecter
+  *@argc: arg counter
+  *
+  *Return - 1 if have a symbols  0 if all right
+  */
+int main(int argc, char **argv)
+{
+	int i = 1;
+	int sum = 0;
+
+	for (; i < argc; i++)
+	{
+		int j = 0;
+		while ( argv[i][j] != '\0')
+		{
+			if (!(isdigit(argv[i][j])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,4 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 int _putchar(char c);
+int _isdigit(int c);
+int _atoi(char *s);
 #endif
