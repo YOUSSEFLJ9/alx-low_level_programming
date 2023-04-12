60dda9679d75b8a003f92623c6917113964097f8
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-12 00:15:29 +0000

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,13 +15,13 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
 	p = malloc(size);
 
 	if (p == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 	{
