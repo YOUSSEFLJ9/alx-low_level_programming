ed2d4292ab9f4e917524db151ba605d369a75d1e
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-12 03:13:30 +0000

    done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,14 +15,16 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0;
 	unsigned int k = 0;
 	char *p;
-	size_t l1 = sizeof(s1);
-	size_t l2 = sizeof(s2);
+	unsigned int l1, l2;
 
 	if (s1 == NULL)
 	s1 = "";
 
 	if (s2 == NULL)
 	s2 = "";
+
+	l1 = strlen(s1);
+	l2 = strlen(s2);
 
 	p = malloc(l1 + l2 + 1);
 	if (p == NULL)
