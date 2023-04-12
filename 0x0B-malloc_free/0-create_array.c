7247f1379cae653d179421c19b629353066eb671
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-12 00:07:12 +0000

    done

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+  **create_array - function creat array of char
+  *@size: the size of array
+  *@c: the variable c
+  *
+  *Return: NULL if size 0/ it fails or pinter of array
+  */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(size);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= size; i++)
+	{
+		p[i] = c;
+	}
+	return (*p);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+#this is ther read me file of 0x0B-malloc_free#
