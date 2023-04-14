8b87c94030c6857ac09175ca8a4a08817efe09de
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-14 00:12:58 +0000

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *malloc_checked - function to allocat memory
+ *
+ * @b: number of bytes to allocate
+
+ *
+
+ * Return: a pointer to the allocated memory
+
+ */
+
+void *malloc_checked(unsigned int b)
+	{
+
+	void *pp;
+
+	pp = malloc(b);
+
+	if (pp == NULL)
+
+		exit(98);
+
+	return (pp);
+
+}
+
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,6 @@
+#this is the read me file#
+
+##0-malloc_checked.c:##
+function that allocates memory using malloc.
+
+
