583c21b382569988ec96fc244bc2402581c0d7df
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-14 20:14:56 +0000

    done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,13 +4,13 @@
 /**
   *string_nconcat - function concatenates two strings
   *@s1: first string
-  *@s2: secound string 
+  *@s2: secound string
   *@n: byt of s2;
   *Return: if fail NULL . or pointer
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *string;
+	char *string;
 	unsigned int i = 0, j = 0, lenght = 0, lenght2 = 0;
 
 	while (s1 && s1[lenght])
@@ -19,9 +19,9 @@ char *string;
 		lenght2++;
 
 	if (n < lenght2)
-		string = malloc(sizeof(char) * (len1 + n + 1));
+		string = malloc(sizeof(char) * (lenght + n + 1));
 	else
-		string = malloc(sizeof(char) * (len1 + len2 + 1));
+		string = malloc(sizeof(char) * (lenght + lenght2 + 1));
 
 	if (!string)
 		return (NULL);
