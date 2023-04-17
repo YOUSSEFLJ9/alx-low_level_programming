c7442ce26931cd659dc887b74cdb19058bf4c6a9
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-17 23:59:18 +0000

    done with dogs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,39 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
+  *Mystlen - count lenght of string
+  *@string: string
+  *Return: lenght
+  */
+int Mystlen(char *string)
+{
+	int i = 0;
+
+	while (string[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+  *Mystcopy - fuction to copy strings
+  *
+  *@dest: destnation vr
+  *@src: source vr
+  *Return: dest full of src
+  */
+char *Mystcopy(char *dest, char *src)
+{
+	int len = Mystlen(src);
+	int i;
+
+	for (i = 0 ; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
   *new_dog - this fuction fill variable of dog_t from the struct dog
   *
   *@name: dogs name
@@ -12,16 +45,26 @@
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog2;
+	dog_t *dog = malloc(sizeof(struct dog));
 
-		dog2 = malloc(sizeof(struct dog));
-	if (dog2 == NULL)
+	if (dog == NULL)
 		return (NULL);
 
-
-	(*dog2).name = name;
-	dog2->age = age;
-	dog2->owner = owner;
-
-	return (dog2);
+	dog->name = malloc(Mystlen(name) + 1);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	Mystcopy(dog->name, name);
+	dog->owner = malloc(Mystlen(owner) + 1);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	Mystcopy(dog->owner, owner);
+	dog->age = age;
+	return (dog);
 }
