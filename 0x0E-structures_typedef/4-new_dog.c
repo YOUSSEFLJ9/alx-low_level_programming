4bf651d537a3d1d44d101e250a2cd9722ac76751
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-17 23:15:32 +0000

    new dog is done

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+  *new_dog - this fuction fill variable of dog_t from the struct dog
+  *
+  *@name: dogs name
+  *@age: dog age
+  *@owner: dog owner
+  *
+  *Return: dog struct
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog2;
+
+	dog2 = malloc(sizeof(struct dog));
+	(*dog2).name = name;
+	dog2->age = age;
+	dog2->owner = owner;
+
+	return (dog2);
+}
