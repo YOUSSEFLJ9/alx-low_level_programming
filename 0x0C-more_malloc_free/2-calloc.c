cd5edc6afc6ac7b12c197b6194a4f901a07be053
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-18 12:43:43 +0000

    done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,8 +15,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *pointer = malloc(nmemb * size);
 
 	if (nmemb == 0 || size == 0)
+	{
+		free(pointer);
 		return (NULL);
-
+	}
 	if (pointer == NULL)
 	{
 		free(pointer);
