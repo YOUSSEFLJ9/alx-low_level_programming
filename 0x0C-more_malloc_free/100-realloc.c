41a7b3c37441e78d4b9b31018bfbd4483ff97d39
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-18 20:28:51 +0000

    test

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *new;
 	int i;
 
-	if (new_size == 0 || ptr != NULL)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
@@ -22,12 +22,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		new = malloc(new_size);
-		return (NULL);
+		return (new);
 	}
 	new = malloc(new_size);
 	if (new == NULL)
 	{
-		free(new);
+		free(ptr);
 		return (NULL);
 	}
 	if (old_size < new_size)
