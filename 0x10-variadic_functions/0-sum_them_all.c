87d2efc28a6006936767c0f9d3b7100609fc200c
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-21 00:33:37 +0000

    variadic are fun

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+  *sum_them_all - this function going to sum all the parameters
+  *@n: NUmber pf parameters
+  *Return: sum of ALL
+  */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int x, sum = 0;
+	va_list counterptr;
+
+	if (n == 0)
+		return (0);
+
+	va_start(counterptr, n);
+	for(x = 0; x < n; x++)
+	{
+		sum = sum + va_arg(counterptr, int);
+	}
+	va_end(counterptr);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,3 @@
+##this file about project 0x10. C - Variadic functions##
+va_start, va_arg and va_end
+
