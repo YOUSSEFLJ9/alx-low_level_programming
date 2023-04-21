8731108d50ca6b15c0f2a908f11bc4685a401ff7
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-21 15:59:55 +0000

    cool

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,19 +8,17 @@
   */
 void print_all(const char * const format, ...)
 {
-
 	int i = 0;
 	char *string;
 	char *sep = ", ";
 	va_list data;
+	double d;
 
 	va_start(data, format);
-
 	if (format == 0)
 	{
 		exit(0);
 	}
-
 	while (format[i] != '\0')
 	{
 		switch (format[i])
@@ -32,16 +30,16 @@ void print_all(const char * const format, ...)
 				printf("%d%s", va_arg(data, int), sep);
 				break;
 			case 'f':
-				printf("%f%s", va_arg(data, double), sep);
+				d = va_arg(data, double);
+				printf("%f%s", d, sep);
 				break;
 			case 's':
 				string = va_arg(data, char*);
-					if (*string == '\0')
+					if (string == NULL || *string == '\0')
 						string = "(nil)";
 				printf("%s%s", string, sep);
 				break;
 		}
-
 		i++;
 		if (format[i + 1] == 0)
 			sep = "";
