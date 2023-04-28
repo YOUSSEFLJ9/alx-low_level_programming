b59e5b3f453e792fcdd757bc8e2096b1a5d3815e
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-28 23:24:34 +0100

    outside the main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,8 @@
 #include "lists.h"
+void first(void)__attribute__((constructor));
 
-printf("%s", "You're beat! and yet, you must allow,\n");
-printf("%s", "I bore my house upon my back!\n");
+void first(void)
+{
+	printf("%s", "You're beat! and yet, you must allow,\n");
+	printf("%s", "I bore my house upon my back!\n");
+}
