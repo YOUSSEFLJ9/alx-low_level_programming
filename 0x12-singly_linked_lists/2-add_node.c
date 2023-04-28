714d4c56521bb575284469dcb33db7ed8bd39694
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-28 17:32:23 +0100

    add-node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,11 +1,17 @@
 #include "lists.h"
+/**
+  *add_node - this fumction add new node at rhe beginning of the list
+  *@head: is a pointer to point to NULL if the list empty or the next nude
+  *@str: pointer to string we want to add it
+  *Return: the nude who we add it
+  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *node;
 	char *strdp;
 
 	node = malloc(sizeof(list_t));
-	if(node == NULL)
+	if (node == NULL)
 		return (NULL);
 
 	strdp = strdup(str);
