480cd17f75bf8a8c18f15f51e1f5588dc7154150
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-28 23:29:26 +0100

    outside the main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 void first(void)__attribute__((constructor));
 
+/**
+ *first - this fuction print before the main start
+ */
 void first(void)
 {
 	printf("%s", "You're beat! and yet, you must allow,\n");
