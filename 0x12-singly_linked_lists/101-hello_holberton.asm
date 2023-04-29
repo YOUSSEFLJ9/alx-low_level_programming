55cca16a4f802b2dfa65d2212a902e0999418296
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-29 01:16:28 +0100

    we can print using 10 or 0xA 0xa

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,20 +3,17 @@
 ;Date: 2023-apr-29
 
 section .data
-    p db "Hello, Holberton",10
+
 
 section .text
     global main
     extern printf
 
 main:
-    push rbp
-    mov rbp, rsp
+    mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+     format: db `Hello, Holberton\n`,0
 
-    lea rdi, [p]
-    xor eax, eax
-    call printf
-
-    mov rsp, rbp
-    pop rbp
-    ret
