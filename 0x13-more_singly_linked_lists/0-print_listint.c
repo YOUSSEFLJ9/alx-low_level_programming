653250849257263ae88853c1310855af767cc862
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-30 16:24:00 +0100

    fix betty doc/style

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,9 @@
 #include "lists.h"
+/**
+ *print_listint - this function print all node in listint stuct
+ *@h :is the header
+ *Return: the counter of how many node has printed
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
