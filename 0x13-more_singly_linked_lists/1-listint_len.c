d1427fb317fb857479119714a0abd8f2c48c1e6a
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-04-30 16:41:44 +0100

    len of list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *listint_len -this function count the nude of listint list
+ *@h: the header of listint list.
+ *Return: the number of element in the list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
