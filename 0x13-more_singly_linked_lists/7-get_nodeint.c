397d02f6381e9e77ef85a027e51b7cdba4a705bd
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-05-01 20:16:00 +0100

    task done

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,10 +10,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int n;
 	listint_t *node;
 	node = head;
+	if (node == NULL)
+		return (NULL);
 	for (n = 0; (index > n) && (node != NULL); n++)
 		node = node->next;
 	if (node != NULL)
 		return node;
 
-	return (NULL);
+	else
+		return (NULL);
 }
