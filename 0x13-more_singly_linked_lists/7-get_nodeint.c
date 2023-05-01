e67fc502cfbfa5b7ed51d39435834b1c381cedef
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-05-01 20:14:30 +0100

    task done

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+ *get_nodeint_at_index - this function return the n of the node that repesent index
+ *@head: is point the head of the list
+ *@index: is the order of the node to be returned
+ *Return: the n's node of index or NULL
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int n;
