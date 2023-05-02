07dcd54650d8c75452716f754ade2147abbdabab
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-05-02 21:49:50 +0100

    task done

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,9 +13,9 @@ void free_listint2(listint_t **head)
 
 	while (*head)
 	{
-		*head = nude->next;
-		free(nude);
-		nude = *head;
+		nude = (*head)->next;
+		free(*head);
+		*head = nude;
 	}
 	*head = NULL;
 
