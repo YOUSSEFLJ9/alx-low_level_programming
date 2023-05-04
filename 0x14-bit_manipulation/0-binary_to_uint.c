fb9509a0f934bdd123179aba736448111eff95a5
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-05-04 22:39:21 +0100

    task done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,5 @@
 #include "main.h"
 /**
- *power - this function calculat the power
- *@N: the number
- *@c: the times of power
- *Return: the result
- */
-unsigned int power(int N, int c)
-{
-	int sum = 1;
-	int i;
-
-	if (c == 0)
-	{
-		sum = 1;
-		return (sum);
-	}
-	else
-	{
-		for (i = 1; i <= c; i++)
-		{
-			sum = sum * N;
-		}
-		return (sum);
-	}
-}
-
-/**
  *binary_to_uint - this convirtour to int
  *@b: pointer to stirng binary
  *Return: the number converted
@@ -41,14 +15,13 @@ unsigned int binary_to_uint(const char *b)
 
 	while (*b != '\0')
 	{
-		if (*b == '0')
+		if (*b== '1' || *b == '0')
 		{
-			c++;
-		}
-		else if (*b == '1')
-		{
-			rt += power(2, c);
-			c++;
+	
+
+		
+			c = *b - '0';
+			rt = rt * 2 + c;
 		}
 		else
 		{
