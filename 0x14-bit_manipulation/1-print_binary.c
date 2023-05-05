e7b91bec081f8efaca3f27ce67d7732f14d76246
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-05-05 01:52:42 +0100

    task done

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,7 @@ void print_binary(unsigned long int n)
 	unsigned long int m = 1;
 
 	if (n == 0)
-		_putchar('0');
+		putchar('0');
 	while (m <= n)
 		m = m << 1;
 	if (m > n)
@@ -18,9 +18,9 @@ void print_binary(unsigned long int n)
 	while (m)
 	{
 		if (n & m)
-			_putchar('1');
+			putchar('1');
 		else
-			_putchar('0');
+			putchar('0');
 		m = m >> 1;
 	}
 }
