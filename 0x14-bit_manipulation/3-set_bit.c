b8ef5798326a753dbfccae3cd855e6a784da5ae5
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-05-05 02:35:39 +0100

    task done

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *set_bit - this fucntion site bit at indix in 1
+ *@n: the number wher to be sets
+ *@index: the indix in the place to be change
+ *Return: 1 if its work or -1 if not.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index <= 64)
+		return (-1);
+
+	*n |= (1UL << index);
+
+	return (1);
+}
