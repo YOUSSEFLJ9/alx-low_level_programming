6a7b55d8aa80389b6bf22c99d214130abc2ac917
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-05-10 00:09:18 +0100

    task done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+/**
+ *read_textfile - this function read text file and print it
+ *@filename: is the actual file name
+ *@letters: this is the NUmber of letters should read and print
+ *Return: the NUmber of letters printed or 0 if cant onpen or null...
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t bytes_read = 0;
+	int fd;
+	char buf[1024];
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	return (0);
+
+	bytes_read = read(fd, buf, letters);
+
+	if (bytes_read == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	if (write(STDOUT_FILENO, buf, bytes_read) == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	close(fd);
+	return (bytes_read);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+this file is the read me file of 0x15-file_io
