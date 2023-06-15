3bbb6a82cca3076af9cb6007e2f090da93b84406
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-06-15 18:13:39 +0100

    pritlistint

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ *print_dlistint - this funtion print all elements of a dlisint_t list
+ *@h: the node in a some postion.
+ *Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	dlistint_t *next = (dlistint_t *)h;
+	dlistint_t *back = (dlistint_t *)h;
+
+	while ((back = back->prev))
+	{
+		printf("%d\n", back->n);
+		count++;
+	}
+	while (next)
+	{
+		printf("%d\n", next->n);
+		count++;
+		next = next->next;
+	}
+	return (count);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,10 @@
+this file contine the files of the project 0x17-doubly_linked_lists
+-
+-
+-
+-
+-
+-
+-
+-
+-
