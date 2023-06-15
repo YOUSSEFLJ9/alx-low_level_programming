5d9e8c3929ba9f3c87ee9111aabb08d142b27d84
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-06-15 18:25:21 +0100

    handl h is NULL

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,6 +10,9 @@ size_t print_dlistint(const dlistint_t *h)
 	dlistint_t *next = (dlistint_t *)h;
 	dlistint_t *back = (dlistint_t *)h;
 
+	if (h == NULL)
+		return (0);
+
 	while ((back = back->prev))
 	{
 		printf("%d\n", back->n);
