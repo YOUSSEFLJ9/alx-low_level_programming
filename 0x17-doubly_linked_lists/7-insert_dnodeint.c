dfaff9d6f20c45d1e9421e11db754877f10cddfe
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-06-15 23:50:15 +0100

    task done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,29 +17,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
 	node = *h;
-	while (node)
+	while (node )
 	{
-		if (!node)
-			return (NULL);
-		if (idx == index)
-		{
-			if (!node)
-				return (add_dnodeint_end(h, n));
-			node->prev->next = new_node;
-			new_node->prev = node->prev;
-			new_node->next = node;
-			node->prev = new_node;
-			return (new_node);
-		}
+		if(index == idx)
+			break;
 		index++;
 		node = node->next;
 	}
+	if (idx == index)
+	{
+		if (!node)
+			return (add_dnodeint_end(h, n));
+
+		new_node = malloc(sizeof(dlistint_t));
+		if (!new_node)
+			return (NULL);
+		new_node->n = n;
+		node->prev->next = new_node;
+		new_node->prev = node->prev;
+		new_node->next = node;
+		node->prev = new_node;
+		return (new_node);
+	}
+
 	return (NULL);
 }
