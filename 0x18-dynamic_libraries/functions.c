ad563fb3cae42d2ff2f359288590fd78084437b3
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-06-20 00:55:35 +0100

    task done

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,51 @@
+#include "list.h"
+/**
+ *add - this function return add of two number
+ *@a: int number
+ *@b: int number
+ *Return: the sam
+ */
+int add(int a, int b)
+{
+	return (a + (b));
+}
+/**
+ *sub - this function return add of two number
+ *@a: int number
+ *@b: int number
+ *Return: the substraction
+ */
+int sub(int a, int b)
+{
+	return (a - (b));
+}
+/**
+ *mul - this function return multplication of two number
+ *@a: int number
+ *@b: int number
+ *Return: the mul
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *div - this function return div of two number
+ *@a: int number
+ *@b: int number
+ *Return: the dev
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *mod - this function return modulo of two number
+ *@a: int number
+ *@b: int number
+ *Return: the modulo
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
