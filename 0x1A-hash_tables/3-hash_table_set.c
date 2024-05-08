ebb2d603a3cc1b325a9335819a7c4e9ae2fae5f7
YOUSSEFLJ9 <youssefmomen@outlook.com>
2024-05-09 00:54:25 +0100

    task is done

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+  * hash_table_set - give the index of a key.
+  * @ht: the hash table
+  * @key: the key string
+  * @value: is the key
+  * Return: 1 if success otherwise 0
+  */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long	j;
+	hash_node_t		*new_node;
+	hash_node_t		*node;
+
+	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = (char *)key;
+	new_node->value = (char *)value;
+	new_node->next = NULL;
+	j = key_index((const unsigned char *) key, ht->size);
+	if (!ht->array[j])
+		ht->array[j] = new_node;
+	else
+	{
+		node = ht->array[j];
+		ht->array[j] = new_node;
+		new_node->next = node;
+	}
+	return (1);
+}
