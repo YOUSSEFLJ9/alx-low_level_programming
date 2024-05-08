2a119319b509e4789e3cb52db2b705ad57cce72d
YOUSSEFLJ9 <youssefmomen@outlook.com>
2024-05-08 04:21:01 -0700

    new commit

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+  * key_index - give the index of a key.
+  * @key: the key string
+  * @size: the size of the array of the hash table
+  * Return: the index at which the key pair shold be stored in the array of the hash tabe
+  */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	
+}
