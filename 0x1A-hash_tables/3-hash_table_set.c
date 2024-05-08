2a60e14df8528c8001656daaf5bbc5f7ad6824d7
YOUSSEFLJ9 <youssefmomen@outlook.com>
2024-05-09 00:57:21 +0100

    task is done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,6 +12,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t		*new_node;
 	hash_node_t		*node;
 
+	if (!ht || !key  || !*key || !value)
+		return (0);
+
 	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
