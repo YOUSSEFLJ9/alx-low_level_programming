a0df46764aadcc4735884079d7124a5f66bdad26
YOUSSEFLJ9 <youssefmomen@outlook.com>
2024-05-09 12:51:29 +0100

    all good if i did count the leak lol

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+  * hash_table_print - function that remove the hash tabe.
+  * @ht: the hash table
+  * Return: do not do anything if the ht is NULL
+  */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long i;
+	hash_node_t	*node;
+
+	if (!ht)
+		return;
+	i = 0;
+	while (i < ht->size)
+	{
+		while (ht->array[i])
+		{
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			node = ht->array[i];
+			ht->array[i] = ht->array[i]->next;
+			free(node);
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
+	ht = NULL;
+}
