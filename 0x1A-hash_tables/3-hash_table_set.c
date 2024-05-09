eca3a810f3f14d8fc55b0ac406d4b4689c7b0685
YOUSSEFLJ9 <youssefmomen@outlook.com>
2024-05-09 01:41:06 +0100

    task is done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,31 +10,31 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long	j;
 	hash_node_t		*new_node;
+	char			*new_value;
+	char			*new_key;
 
 	if (!ht || !key  || !*key || !value)
 		return (0);
-
 	j = key_index((const unsigned char *) key, ht->size);
 	if (ht->array[j] && !strcmp(ht->array[j]->key, key))
 	{
-		free(ht->array[j]->key);
 		free(ht->array[j]->value);
-		ht->array[j]->key = strdup(key);
 		ht->array[j]->value = strdup(value);
-		if (!ht->array[j]->key)
-			return (0);
 		if (!ht->array[j]->value)
-		{
-			free(ht->array[j]->key);
 			return (0);
-		}
 		return  (1);
 	}
+	new_key = strdup(key);
+	if (!new_key)
+		return (0);
+	new_value = strdup(value);
+	if (!new_value)
+		return (free(new_key), 0);
 	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (!new_node)
-		return (0);
-	new_node->key = (char *)key;
-	new_node->value = (char *)value;
+		return (free(new_value), free(new_key), 0);
+	new_node->key = new_key;
+	new_node->value = new_value;
 	new_node->next = ht->array[j];
 	ht->array[j] = new_node;
 	return (1);
