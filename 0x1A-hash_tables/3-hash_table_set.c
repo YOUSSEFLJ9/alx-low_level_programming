86b986e674f5b67b96b77a8734e2cdd31a9ab867
YOUSSEFLJ9 <youssefmomen@outlook.com>
2024-05-09 01:07:25 +0100

    task is done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,14 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new_node->key = (char *)key;
 	new_node->value = (char *)value;
-	new_node->next = NULL;
-	if (!ht->array[j])
-		ht->array[j] = new_node;
-	else
-	{
-		node = ht->array[j];
-		ht->array[j] = new_node;
-		new_node->next = node;
-	}
+	new_node->next = ht->array[j];
+	ht->array[j] = new_node;
 	return (1);
 }
