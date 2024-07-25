9d391b0ebe2d981dc2153047f56da085cdfeb0e1
YOUSSEFLJ9 <youssefmomen@outlook.com>
2024-07-25 11:20:44 +0100

    python is cool

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+5-island_perimeter
+is a fuction to technival interview prepartion : that return the peri,ter of the island described in grid variable
+"""
+def island_perimeter(grid):
+    """
+    return the p, of the island
+
+    """
+    width = len(grid)
+    height = len(grid[0])
+    size = 0
+    edge = 0
+    for row in range(height-1):
+        for col in range(width-1):
+            if grid[row][col] == 1:
+                size += 1
+                if (col > 0 and grid[row][col - 1] == 1):
+                    edge += 1
+                if (row > 0 and grid[row - 1][col] == 1):
+                    edge += 1
+    return size * 4 - edge * 2
