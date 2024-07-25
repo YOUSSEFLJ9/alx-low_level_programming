0dffb49f65b8f7d77c28d2ea2d7343350d0a840b
YOUSSEFLJ9 <youssefmomen@outlook.com>
2024-07-25 11:51:47 +0100

    now good

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,19 +5,23 @@ is a fuction to technival interview prepartion : that return the peri,ter of the
 """
 def island_perimeter(grid):
     """
-    return the p, of the island
+    return the p, of the island.
 
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
     """
     height = len(grid)
     width = len(grid[0])
     size = 0
     edge = 0
-    for row in range(height - 1):
-        for col in range(width - 1):
+    for row in range(height):
+        for col in range(width):
             if grid[row][col] == 1:
                 size += 1
                 if (col > 0 and grid[row][col - 1] == 1):
                     edge += 1
                 if (row > 0 and grid[row - 1][col] == 1):
                     edge += 1
-    return size * 4 - edge * 2
+    return (size * 4) - (edge * 2)
